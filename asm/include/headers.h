bf0c304cc353a468af56b1d39924d2159c77dd43
Adrien LENGLET <adrien.lenglet3@gmail.com>
2019-05-08 17:24:18 +0200

    Add asm base

---
+++ asm/include/headers.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** (´ε｀；)
+** File description:
+** headaaaas
+*/
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
---
+++ asm/src/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2018
+** (´ε｀；)
+** File description:
+** main
+*/
+
+#include "headers.h"
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    return 0;
+}
