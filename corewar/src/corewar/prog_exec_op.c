0a91def2528fc07536102449ab84ea29900aeb35
CorentinRenard <corentin.renard@epitech.eu>
2019-05-19 12:37:18 +0200

    Add st, add, sub

--- corewar/src/corewar/prog_exec_op.c
+++ corewar/src/corewar/prog_exec_op.c
@@ -20,11 +20,33 @@ static void ld(prog_t *prog)
 {
     int32_t arg0_val = vm_mem_ref_read(prog->op.arg[0]);
 
-    if (!arg0_val)
-        prog->carry = 1;
-    else
-        prog->carry = 0;
+    prog->carry = (arg0_val == 0);
     vm_mem_ref_write(prog->op.arg[1], arg0_val);
+}
+
+static void st(prog_t *prog)
+{
+    int32_t arg0_val = vm_mem_ref_read(prog->op.arg[0]);
+
+    vm_mem_ref_write(prog->op.arg[1], arg0_val);
+}
+
+static void add(prog_t *prog)
+{
+    int32_t arg0_val = vm_mem_ref_read(prog->op.arg[0]);
+    int32_t arg1_val = vm_mem_ref_read(prog->op.arg[1]);
+
+    prog->carry = (arg0_val + arg1_val) == 0;
+    vm_mem_ref_write(prog->op.arg[2], (arg0_val + arg1_val));
+}
+
+static void subb(prog_t *prog)
+{
+    int32_t arg0_val = vm_mem_ref_read(prog->op.arg[0]);
+    int32_t arg1_val = vm_mem_ref_read(prog->op.arg[1]);
+
+    prog->carry = (arg0_val - arg1_val) == 0;
+    vm_mem_ref_write(prog->op.arg[2], (arg0_val - arg1_val));
 }
 
 void prog_exec_op(prog_t *prog)
