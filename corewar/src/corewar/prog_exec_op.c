b958ccc11219ce7dc066b44ec3dfcc078b0892a0
CorentinRenard <corentin.renard@epitech.eu>
2019-05-19 12:09:54 +0200

    Add ld

--- corewar/src/corewar/prog_exec_op.c
+++ corewar/src/corewar/prog_exec_op.c
@@ -16,6 +16,17 @@ static void live(prog_t *prog)
             _vm.progs.prog[i].life = 0;
 }
 
+static void ld(prog_t *prog)
+{
+    int32_t arg0_val = vm_mem_ref_read(prog->op.arg[0]);
+
+    if (!arg0_val)
+        prog->carry = 1;
+    else
+        prog->carry = 0;
+    vm_mem_ref_write(prog->op.arg[1], arg0_val);
+}
+
 void prog_exec_op(prog_t *prog)
 {
     switch (prog->op.code) {
