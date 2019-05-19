809581eb6a18314fd261f27975b6b0a3726e8b81
Adrien LENGLET <adrien.lenglet3@gmail.com>
2019-05-19 13:37:56 +0200

    Add nomod mem instr

--- corewar/src/corewar/vm_mem.c
+++ corewar/src/corewar/vm_mem.c
@@ -48,14 +48,43 @@ void vm_mem_ref_write(vm_mem_ref_t ref, int32_t value)
     }
 }
 
-int32_t vm_mem_ref_read_rel(vm_mem_ref_t ref, int32_t offset)
+int32_t vm_mem_ref_read_nomod(vm_mem_ref_t ref)
 {
-    ref.ptr = (ssize_t)ref.ptr + offset;
-    return vm_mem_ref_read(ref);
+    switch (ref.type) {
+    case VM_MEM_REF_MAIN:
+        return *((int32_t*)ref.ptr);
+    case VM_MEM_REF_VM_ABS:
+        if (ref.is_short)
+            return vm_read_uint16(ref.ptr);
+        else
+            return vm_read_uint32(ref.ptr);
+    case VM_MEM_REF_VM_REL:
+        if (ref.is_short)
+            return vm_read_uint16(_vm.prog_running->op.pc + ref.ptr);
+        else
+            return vm_read_uint32(_vm.prog_running->op.pc + ref.ptr);
+    default:
+        return 0;
+    }
 }
 
-void vm_mem_ref_write_rel(vm_mem_ref_t ref, int32_t offset, int32_t value)
+void vm_mem_ref_write_nomod(vm_mem_ref_t ref, int32_t value)
 {
-    ref.ptr = (ssize_t)ref.ptr + offset;
-    vm_mem_ref_write(ref, value);
+    switch (ref.type) {
+    case VM_MEM_REF_MAIN:
+        *((int32_t*)ref.ptr) = value;
+        break;
+    case VM_MEM_REF_VM_ABS:
+        if (ref.is_short)
+            return vm_write_uint16(ref.ptr, value);
+        else
+            return vm_write_uint32(ref.ptr, value);
+    case VM_MEM_REF_VM_REL:
+        if (ref.is_short)
+            return vm_write_uint16(
+            _vm.prog_running->op.pc + ref.ptr, value);
+        else
+            return vm_write_uint32(
+            _vm.prog_running->op.pc + ref.ptr, value);
+    }
 }
